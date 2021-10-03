6041d7c14631bb3c7549d0239fea778ca4c91ce8
Emily Gleeson <engleeson@gmail.com>
2021-10-03 15:10:13 -0400

    successfully returned data from api, added check for adding employees manually or from api

--- Program.cs
+++ Program.cs
@@ -5,37 +5,20 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-    static List<Employee> GetEmployees ()
-        {
-            //returns a list of strings
-          List<Employee> employees = new List<Employee>();
-            while (true)
-            {
-            Console.WriteLine("Enter first name: (leave empty to exit)");
-            string firstName = Console.ReadLine();
-                if (firstName == "")
-                 {
-                  break;
-                 }
-
-                Console.Write("Enter last name: ");
-                string lastName = Console.ReadLine();
-
-                Console.Write("Enter ID: ");
-                int id = Int32.Parse(Console.ReadLine());
-
-                Console.Write("Enter Photo URL: ");
-                string photoUrl = Console.ReadLine();
-
-          Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-          employees.Add(currentEmployee);
-          }
-         return employees;
-        }
+    
 
     static void Main(string[] args)
         {
-          List<Employee> employees = GetEmployees();
+          List<Employee> employees;
+          Console.WriteLine("Welcome to Catworx Employee Badge Maker!\nDo you want to create badges manually or automatically based on our employee database?\nEnter m or M for manual creation - default is automatic.");
+          string answer = Console.ReadLine();
+          if (answer == "m" || answer == "M")
+          {
+            employees = PeopleFetcher.GetEmployees();
+          }
+          else {
+            employees = PeopleFetcher.GetFromApi();
+          }
 
           Util.PrintEmployees(employees);
 
